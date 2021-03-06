57db173062447cb5592f3d1073311c505aa0bb66
novomestan <78145236+novomestan@users.noreply.github.com>
2021-03-06 20:40:33 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+function modracervena () {
+    strip.show()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+}
+function cervenamodra () {
+    strip.show()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Red))
+}
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    cervenamodra()
+    music.playTone(392, music.beat(BeatFraction.Half))
+    basic.pause(200)
+    modracervena()
+    music.playTone(523, music.beat(BeatFraction.Half))
+    basic.pause(200)
 })
